727aec005ebac485d0a39c364ce9249088ec0fc5
BlyaHoblee <59060848+hoble@users.noreply.github.com>
2020-11-20 16:51:26 +0300

    Update sendm.php

--- sendm.php
+++ sendm.php
@@ -19,5 +19,6 @@
     $subject = "=?utf-8?B?".base64_encode("Сообщение от сайта")."?=";
     $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html;charset=utf-8\r\n";
 
-    mail('matveylazarev516@gmail.com', $subject, $message, $headers)
+    mail('matveylazarev516@gmail.com', $subject, $message, $headers);
+    header('Location: index.php');
 ?>
